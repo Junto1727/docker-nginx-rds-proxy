0b184f3ab7c2752305415965c954604d663faa49
Rob <robertchristopherdev11@gmail.com>
2017-03-23 18:11:16 -0400

    Added image entrypoint.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,4 +3,5 @@
 sed -i 's/<DATABASE_SERVER>/'"$DATABASE_HOST"'/g' /etc/nginx/nginx.conf
 sed -i 's/<DATABASE_PORT>/'"$DATABASE_PORT"'/g' /etc/nginx/nginx.conf
 
+echo "Restarting nginx"
 service nginx start
